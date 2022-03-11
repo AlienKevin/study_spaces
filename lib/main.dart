09c5371a29dcf18aca926f1032710c905521a8f8
Kevin Li <kevinli020508@gmail.com>
2022-03-11 15:09:21 -0500

    Show library cards in home

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Study spaces',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Opening Now'),
+      home: MyHomePage(title: 'Opening Now'),
     );
   }
 }
 
+class StudySpace {
+  final String name;
+  final int hoursStart;
+  final int hoursEnd;
+  final String pictureUrl;
+
+  StudySpace(
+      {required this.name,
+      required this.hoursStart,
+      required this.hoursEnd,
+      required this.pictureUrl});
+}
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -48,68 +43,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<StudySpace> studySpaces = [
+    StudySpace(
+        name: "Art, Architecture, and Engineering Library",
+        hoursStart: 9,
+        hoursEnd: 22,
+        pictureUrl:
+            "https://www.lib.umich.edu/static/542b6db6cb4cb1f683c44314617cebf8/fddf6/duderstadt2-june2019_edited.jpg"),
+    StudySpace(
+        name: "Hatcher Library",
+        hoursStart: 10,
+        hoursEnd: 21,
+        pictureUrl:
+            "https://www.lib.umich.edu/static/db8149c1851a919702b5eeda601def8e/fddf6/hatcher-5-june2019_edited.jpg"),
+    StudySpace(
+        name: "Shapiro Library",
+        hoursStart: 9,
+        hoursEnd: 20,
+        pictureUrl:
+            "https://www.lib.umich.edu/static/a702abfd347b4a1f3591d9f4ad572594/f9008/shapiro-4-june2019_edited.jpg"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          showFeaturedListItem(studySpaces[0]),
+          ...studySpaces.sublist(1).map(showListItem).toList(),
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: showFilters,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void showFilters() {}
+
+  Widget showListItem(StudySpace studySpace) {
+    return Card(
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      ListTile(
+        title: Text(studySpace.name),
+        subtitle: Text(studySpace.hoursStart.toString() +
+            "-" +
+            studySpace.hoursEnd.toString()),
+      ),
+    ]));
+  }
+
+  Widget showFeaturedListItem(StudySpace studySpace) {
+    return Card(
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          Image.network(studySpace.pictureUrl),
+          ListTile(
+            title: Text(studySpace.name),
+            subtitle: Text(studySpace.hoursStart.toString() +
+                "-" +
+                studySpace.hoursEnd.toString()),
+          ),
+        ]),
+        margin: const EdgeInsets.all(10));
   }
 }
